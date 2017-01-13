1be8ed00a26fffa0d33d1b2b99306d696f3fc91a
/usr/bin/nano <support@beget.ru>
2017-01-13 16:26:17 +0300

    Google Maps API simple map,developer on dobroad very very bad programmer

--- page/index.php
+++ page/index.php
@@ -1,33 +1,67 @@
-<?php $Title = "Dobroad"; include_once("blocks/header.php");$_SESSION['ucity']='ufa';?>
-<div id="map" style="height: 500px;"></div>
-<script type="text/javascript">
-if(navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(function(position) {
-          var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-          var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: position.coords.latitude, lng: position.coords.longitude},
-            zoom: 8
-          });
-
-});
-
-} else {
-  alert("Geolocation API не поддерживается в вашем браузере");
+<?php $Title = "Dobroad"; include_once("blocks/header.php");$_SESSION['ucity']='ufa';
+if ($_POST['latitude'] && $_POST['longitude']) {
+  $_SESSION['latitude'] = $_POST['latitude'];
+    $_SESSION['longitude'] = $_POST['longitude'];
 }
-</script>
-<?php
+
 if ($_SESSION['status'] != "login") {
 ?>
 <h1>Добро пожаловать на dobroad.ru</h1>
 <p>Для начала работы авторизуйтесь или <a style="text-decoration:underline;" href="/users/registration">зарегистрируйтесь</a></p>
 <?php } else
-if (!$_SESSION['ucity']) {
-  echo "Подтвердите город";
-}
+if (!$_SESSION['latitude'] && !$_SESSION['longitude']) { ?>
 
-else { ?>
-<button type="button" name="passengers">Ищу пассажиров</button>
-<button type="button" name="drivers">Ищу водителя</button>
-<? }
-include_once("blocks/sidebar.php");?>
+  <script type="text/javascript">
+
+  if(navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+  	function funcBefore () {
+  		$("#information").text ("Ожидание");
+  	}
+
+  	function funcSuccess (data) {
+      location.reload();
+  	}
+
+  	$(document).ready (function () {
+  	$("#enter").bind("click", function () {
+  			$.ajax ({
+  				url: "/index",
+  				type: "POST",
+  				data: {latitude: latitude,longitude: longitude},
+  				dataType: "html",
+  				beforeSend: funcBefore,
+  				success: funcSuccess
+  			});
+  		});
+  	});
+
+  });
+  } else {
+    alert("Geolocation API не поддерживается в вашем браузере");
+  }
+  </script>
+  <form method="POST" action="/">
+  <input type="button" id="enter" value="Проверить местоположение">
+</form>
+<div id="information"></div>
+<?php } else { ?>
+    <div id="map" style="height: 500px;"></div>
+    <script>
+
+    var map;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: <?php echo $_SESSION['latitude'] ?>, lng: <?php echo $_SESSION['latitude']?>},
+        zoom: 8
+      });
+    }
+
+        </script>
+  <?php ;
+};
+
+ include_once("blocks/sidebar.php");?>
